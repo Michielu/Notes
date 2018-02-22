f18af33cf4bd5a861efaafea667acf8c80d87e95
Michielu Menning <mich.menning@gmail.com>
2018-02-22 07:46:45 -0600

    Small react stuff

--- note-react/src/App.js
+++ note-react/src/App.js
@@ -3,6 +3,19 @@ import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+    constructor(props){
+      super(props);
+      this.state = {users:[], text: ""};
+
+      this.addText = this.addText.bind(this);
+    }
+
+
+
+  addText(){
+    this.setState({text:"ahh"});
+  }
+
   render() {
     return (
       <div className="App">
@@ -10,9 +23,13 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <h1 className="App-title">Welcome to React</h1>
         </header>
+        <body>
         <p className="App-intro">
           To get started, edit <code>src/App.js</code> and save to reload.
         </p>
+        <button  class="firstbutton" onCLick={this.addText}>Get users</button>
+        <p class="text-users">{this.state.text}</p>
+        </body>
       </div>
     );
   }
