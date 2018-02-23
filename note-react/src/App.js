8c8d034f70e6de40a7320f436404873c742d5fd5
Michielu Menning <mich.menning@gmail.com>
2018-02-23 07:52:23 -0600

    Start breaking down into components

--- note-react/src/App.js
+++ note-react/src/App.js
@@ -1,19 +1,20 @@
 import React, { Component } from 'react';
+import PopulateData from './populateData';
 import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-    constructor(props){
-      super(props);
-      this.state = {users:[], text: ""};
+  constructor(props) {
+    super(props);
+    this.state = { users: [], text: "" };
 
-      this.addText = this.addText.bind(this);
-    }
+    this.addText = this.addText.bind(this);
+  }
 
 
 
-  addText(){
-    this.setState({text:"ahh"});
+  addText() {
+    this.setState({ text: "ahh" });
   }
 
   render() {
@@ -24,11 +25,11 @@ class App extends Component {
           <h1 className="App-title">Welcome to React</h1>
         </header>
         <body>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+          <p className="App-intro">
+            To get started, edit <code>src/App.js</code> and save to reload.
         </p>
-        <button  class="firstbutton" onCLick={this.addText}>Get users</button>
-        <p class="text-users">{this.state.text}</p>
+          <button class="firstbutton" onCLick={this.addText}>Get users</button>
+          <PopulateData></PopulateData>
         </body>
       </div>
     );
---
+++ note-react/src/populateData/index.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class PopulateData extends React.Component {
+    render() {
+        return (
+            <div className="populate-users">
+                <p class="text-users">a</p>
+            </div>
+        );
+    }
+}
