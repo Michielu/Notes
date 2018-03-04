fe966f23284541e16cf512afc64c68970195d4ed
Michielu Menning <mich.menning@gmail.com>
2018-03-04 10:27:24 -0600

    Make getUsers use ES7 promises

--- note-react/src/NoteButton/retreiveInfo.js
+++ note-react/src/NoteButton/retreiveInfo.js
@@ -1,34 +1,22 @@
 export default getUsers;
-var request = require('request');
 var xhr = new XMLHttpRequest();
 var local = `http://localhost:8000/`
 
-/*async*/ function getUsers() {
-    // return new Promise(resolve => {
-    //     var command = `${local}` + 'u/all';
-    //     console.log(command);
-    //     request(command, function (error, response, body) {
-    //         if (!error && response.statusCode == 200) {
-    //             console.log("body: ", body);
-    //             resolve(body);
-    //         }
-    //     })
-    // });
-
-    return fetch('http://localhost:8000/u/a', {
-        method: 'get', /*mode: "cors"*/
-    }).then((res) => {
+async function getUsers() {
+    try {
+        const res = await fetch('http://localhost:8000/u/a', { method: 'get', /*mode: "cors"*/ });
         if (res.status !== 200) {
             console.log('Looks like there was a problem. Status Code: ' + res.status);
             return;
         }
+        return await convertJson(res);
+    }
+    catch (err) {
+        console.log("Failed to get users: ", err);
+    }
+}
 
-        // Examine the text in the response
-        return res.json().then((data) => {
-            console.log("In join", data);
-            return data;
-        });
-    }).catch((err) => {
-        console.log("Err :", err);
-    });
+async function convertJson(res) {
+    const data = await res.json();
+    return data;
 }
